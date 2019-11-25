aaad9c69cb15cb4d4ce386efb34aeaa0d4b10714
shavit-zipori <57913880+shavit-zipori@users.noreply.github.com>
2019-11-25 18:34:41 +0200

    Add files via upload

---
+++ MainGame.py
@@ -0,0 +1,15 @@
+from GuessGame import MyGame
+game=MyGame("Shavit")
+chosen_game=game.Chose_Game()
+print(chosen_game)
+
+from GuessGame import play
+#playgame=play(DifficultyGame)
+Guess=play.Generate_number('self','DifficultyGame')
+print(Guess)
+#print(c.get_guess_from_user())
+#print(c.compare_results())
+#p=play(a)
+#print(p.difficulty_game)
+#print(p.secret_number)
+
